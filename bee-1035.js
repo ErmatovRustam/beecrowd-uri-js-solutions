778dfa958dfd27d39fc19b2dec33f20fa8cec489
ermatovRustam <rustam.22.97@gmail.com>
2023-05-04 11:19:45 -0700

    solved if else problem

---
+++ bee-1035.js
@@ -0,0 +1,13 @@
+let input = require('fs').readFileSync('./dev/stdin', 'utf8')
+var lines = input.split(' ')
+
+let a = Number(lines[0])
+let b = Number(lines[1])
+let c = Number(lines[2])
+let d = Number(lines[3])
+
+console.log(
+  b > c && d > a && c + d > a + b && c >= 0 && b >= 0 && a % 2 === 0
+    ? 'Valores aceitos'
+    : 'Valores nao aceitos'
+)
